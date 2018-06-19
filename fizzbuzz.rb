0e70f5cfe9d6e5c793f851768dff198f64afee5c
Ave12304 <averyselover@hotmail.com>
2018-06-19 20:29:18 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,10 +1,10 @@
 
 def fizzbuzz(number)
-if number%3 == 0
-  puts "fizz"
+if number%15 == 0
+  puts "fizzbuzz"
 elsif number%5 == 0
 puts "buzz"
-else number%15 == 0
+elsif number%3 == 0
   puts "fizzbuzz"
 else
 end
